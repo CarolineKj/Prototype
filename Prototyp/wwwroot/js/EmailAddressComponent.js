1285183d769abdbb8bc97a9e602771eacb4bbf2e
Caroline Kjellander <cakj2001@student.miun.se>
2022-04-14 22:40:20 +0200

    Add project files.

---
+++ Prototyp/wwwroot/js/EmailAddressComponent.js
@@ -0,0 +1,21 @@
+﻿export function initializeEmailAddressComponent(inputId, spanId) {
+    let inputElement = document.querySelector("#" + inputId);
+    if (inputElement) {
+        inputElement.addEventListener("input", (e) => {
+            console.log("input JavaScript");
+            let input = e.target;
+            var hiddenSpan = document.getElementById(spanId);
+            hiddenSpan.innerHTML = input.value;
+            input.style.width = (hiddenSpan.getBoundingClientRect().width + 5) + "px";
+        });
+
+        inputElement.addEventListener("click", (e) => {
+            e.stopPropagation();
+        });
+
+        console.log("new input JavaScript");
+        var hiddenSpan = document.getElementById(spanId);
+        hiddenSpan.innerHTML = inputElement.value;
+        inputElement.style.width = (hiddenSpan.getBoundingClientRect().width + 5) + "px";
+    }
+}
